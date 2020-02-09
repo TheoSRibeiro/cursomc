fad3cdb1dbeb0bf083c65c5ad560f9096cbe60c5
Theo Ribeiro <theosribeiro@gmail.com>
2020-02-09 14:58:44 -0300

    Criando Operacao de instanciacao no BD

--- src/main/java/com/theoribeiro/cursomc/CursomcApplication.java
+++ src/main/java/com/theoribeiro/cursomc/CursomcApplication.java
@@ -1,13 +1,34 @@
 package com.theoribeiro.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.theoribeiro.cursomc.domain.Categoria;
+import com.theoribeiro.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner{ //utilizar CommanLineRunner para instanciar o BD
+
+	// O REPOSITORY SALVA OS DADOS NO BD
+	@Autowired
+	private CategoriaRepository categoriaRepositpry;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception { //INSTANCIAR O BD
+		
+		Categoria cat1 = new Categoria(null,"Informática");
+		Categoria cat2 = new Categoria(null,"Escritório");
+		
+		categoriaRepositpry.save(Arrays.asList(cat1,cat2));
+		
 	}
 
 }
